5fdc6dd5952818c37781a39d21c8bafd4aab3da9
Narkomic <narkomic@gmail.com>
2026-01-20 00:43:52 +0100

    Add files via upload

--- ct/profilarr.sh
+++ ct/profilarr.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# Source build framework (same style as community-scripts)
 source <(curl -fsSL https://raw.githubusercontent.com/narkomic/pve-scripts/main/misc/build.func)
 
 APP="Profilarr"
@@ -9,25 +8,23 @@ var_ram="${var_ram:-2048}"
 var_disk="${var_disk:-10}"
 var_os="${var_os:-debian}"
 var_version="${var_version:-13}"
+
 # Docker i LXC er mest gnidningsfrit i privileged
 var_unprivileged="${var_unprivileged:-0}"
+
+# Matcher filnavnet: install/profilarr-install.sh (uden .sh)
+var_install="${var_install:-profilarr-install}"
 
 header_info "$APP"
 variables
 color
 catch_errors
 
-function install_app() {
-  msg_info "Installing ${APP}"
-  bash <(curl -fsSL https://raw.githubusercontent.com/narkomic/pve-scripts/main/install/profilarr-install.sh)
-  msg_ok "Installed ${APP}"
-}
-
 start
 build_container
 description
-install_app
+install_script
 
-msg_ok "Completed Successfully!\n"
+msg_ok "Completed Successfully!"
 echo -e "${INFO}${YW} Access URL:${CL}"
 echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:6868${CL}\n"
